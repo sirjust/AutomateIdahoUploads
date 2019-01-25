e08d5c4deb371dc4f93de3fb4741ad44664d8a65
SirJUST <theoperaking@yahoo.com>
2019-01-25 09:32:56 -0500

    fix incorrect course error bug, fix first catch block

--- AutomateIdahoUploads/UploadTask.cs
+++ AutomateIdahoUploads/UploadTask.cs
@@ -9,6 +9,8 @@ using OpenQA.Selenium.Chrome;
 using System.Threading;
 using OpenQA.Selenium.Support.UI;
 using AutomateWashingtonUploads;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Interactions;
 
 namespace AutomateIdahoUploads
 {
@@ -18,7 +20,7 @@ namespace AutomateIdahoUploads
 
         public void inputCompletions(List<Completion> completions)
         {
-            driver = new ChromeDriver(@"C:\Users\SirJUST\source\repos\AutomateIdahoUploads\packages\Selenium.Chrome.WebDriver.2.43\driver");
+            driver = new ChromeDriver(@"../../../packages/Selenium.Chrome.WebDriver.2.43/driver");
             driver.Url = "https://launchpad.cebroker.com/login";
             driver.Manage().Window.Maximize();
             LoginInfo loginInfo = new LoginInfo();
@@ -50,82 +52,114 @@ namespace AutomateIdahoUploads
 
                 DateTime completionDate = new DateTime(year, month, day);
 
-                //try
-                //{
+                try
+                {
 
-                IWebElement courseField = wait.Until<IWebElement>(d=>d.FindElement(By.CssSelector(".input-sm.form-control.input-s-sm")));
-                courseField.SendKeys(courseNumber);
-                courseField.SendKeys(Keys.Return);
+                    IWebElement courseField = wait.Until<IWebElement>(d => d.FindElement(By.CssSelector(".input-sm.form-control.input-s-sm")));
+                    courseField.SendKeys(courseNumber);
+                    Thread.Sleep(3000);
+                    courseField.SendKeys(Keys.Return);
+                    Thread.Sleep(3000);
+                    IWebElement firstVisibleCourse = wait.Until<IWebElement>(d => d.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/ul[1]/li[1]/small[1]/span[2]")));
+                    if(courseNumber == firstVisibleCourse.GetAttribute("innerHTML"))
+                    {
+                        IWebElement manageRoster = wait.Until<IWebElement>(d => d.FindElement(By.PartialLinkText("Manage")));
+                        manageRoster.Click();
+                        IWebElement addButton = wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Add")));
+                        addButton.Click();
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                } catch
+                {
+                    string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}", completion.course, completion.date, completion.license, completion.name);
+                    Logger logger = new Logger();
+                    StreamWriter sw = new StreamWriter(@"log.txt", true);
+                    logger.writeErrorsToLog(errorInfo, sw);
+                    sw.Close();
+                    //then go back to the previous page
+                    driver.Navigate().Refresh();
+                    Thread.Sleep(3000);
+                    continue;
+                }
 
-                IWebElement anchor = wait.Until<IWebElement>(d => d.FindElement(By.PartialLinkText("Manage")));
-                anchor.Click();
-                //}
-                //catch
-                //{
-                //    string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}", completion.course, completion.date, completion.license, completion.name);
-                //    Logger logger = new Logger();
-                //    StreamWriter sw = new StreamWriter(@"log.txt", true);
-                //    logger.writeErrorsToLog(errorInfo, sw);
-                //    sw.Close();
-                //    //then go back to the previous page
-                //    IWebElement goBack = driver.FindElement(By.Id("btnPrev"));
-                //    goBack.Click();
-                //    Thread.Sleep(3000);
-                //    continue;
-                //}
+                try
+                {
+                    Thread.Sleep(3000);
+                    IWebElement stateField = wait.Until<IWebElement>(d => d.FindElement(By.Id("state")));
+                    stateField.Click();
+                    stateField.SendKeys(Keys.ArrowDown);
+                    stateField.SendKeys(Keys.Return);
+                    Thread.Sleep(3000);
 
-                //    // if the completion date is incorrect for the course the program will log it and go to the next completion
-                //    // this occurs with older electrical courses, where the completion date is out of the range of course validity
-                //    try
-                //    {
-                //        IWebElement dateInput = wait.Until<IWebElement>(d=> d.FindElement(By.Id("txtComplDt")));
-                //        dateInput.SendKeys(String.Format("{0:MM/dd/yyyy}", completionDate));
-                //    }
-                //    catch
-                //    {
-                //        string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}", completion.course, completion.date, completion.license, completion.name);
-                //        Logger logger = new Logger();
-                //        StreamWriter sw = new StreamWriter(@"log.txt", true);
-                //        logger.writeErrorsToLog(errorInfo, sw);
-                //        sw.Close();
-                //        //then go back to the previous page
-                //        IWebElement goBack = driver.FindElement(By.Id("btnPrev"));
-                //        goBack.Click();
-                //        Thread.Sleep(3000);
-                //        continue;
-                //    }
+                    IWebElement licenseField = wait.Until<IWebElement>(d => d.FindElement(By.Id("licenseNumber")));
+                    licenseField.SendKeys(license);
+                } catch
+                {
+                    string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}", completion.course, completion.date, completion.license, completion.name);
+                    Logger logger = new Logger();
+                    StreamWriter sw = new StreamWriter(@"log.txt", true);
+                    logger.writeErrorsToLog(errorInfo, sw);
+                    sw.Close();
+                    //then go back to the previous page
+                    IWebElement goBack = driver.FindElement(By.Id("btnPrev"));
+                    courseButton.Click();
+                    Thread.Sleep(3000);
+                    continue;
+                }
 
-                //    IWebElement createRoster = wait.Until<IWebElement>(d=> d.FindElement(By.Id("btnGetRoster")));
-                //    createRoster.Click();
+                try
+                {
+                    IWebElement dateCompletedField = wait.Until<IWebElement>(d => d.FindElement(By.Id("dateCompleted")));
+                    dateCompletedField.Click();
+                    int dateLength = 10;
+                    while (dateLength > 0)
+                    {
+                        dateCompletedField.SendKeys(Keys.Backspace);
+                        dateLength--;
+                    }
 
-                //    try
-                //    {
-                //        IWebElement inputLicense = wait.Until<IWebElement>(d=> d.FindElement(By.Id("txtLicense")));
-                //        inputLicense.SendKeys(license);
-                //        IWebElement findLicensee = wait.Until<IWebElement>(d=> d.FindElement(By.Id("btnPeople")));
-                //        findLicensee.Click();
+                    dateCompletedField.SendKeys(String.Format("{0:MM/dd/yyyy}", completionDate));
+                    dateCompletedField.SendKeys(Keys.Enter);
 
-                //        // next we have to submit the roster
-                //        IWebElement addToRoster = wait.Until<IWebElement>(d=> d.FindElement(By.Id("btnTransferToRoster")));
-                //        addToRoster.Click();
-                //    }
-                //    catch
-                //    {
-                //        string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}",completion.course, completion.date, completion.license, completion.name);
-                //        Logger logger = new Logger();
-                //        StreamWriter sw = new StreamWriter(@"log.txt", true);
-                //        logger.writeErrorsToLog(errorInfo, sw);
-                //        sw.Close();
-                //    }
-                //    finally
-                //    {
-                //        //then go back to the previous page
-                //        IWebElement goBack = wait.Until<IWebElement>(d=> d.FindElement(By.Id("btnPrev")));
-                //        goBack.Click();
-                //    }
-                //    //loop again until the end
-                //}
-                //driver.Close();
+                    IWebElement scrollField = wait.Until<IWebElement>(d => d.FindElement(By.Id("formNav")));
+                    scrollField.Click();
+
+                    Actions actions = new Actions(driver);
+                    actions.SendKeys(OpenQA.Selenium.Keys.End).Build().Perform();
+
+                    Thread.Sleep(3000);
+
+                    IWebElement saveButton = wait.Until<IWebElement>(d => d.FindElement(By.CssSelector(".btn.btn-primary.btn-s-xs.btn-sm")));
+                    saveButton.Click();
+
+                    Thread.Sleep(3000);
+
+                    IWebElement postButton = wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Post")));
+                    postButton.Click();
+
+                    Thread.Sleep(3000);
+
+                    courseButton.Click();
+                } catch
+                {
+                    string errorInfo = String.Format("The following completion encountered an error: {0} | {1} | {2} | {3}", completion.course, completion.date, completion.license, completion.name);
+                    Logger logger = new Logger();
+                    StreamWriter sw = new StreamWriter(@"log.txt", true);
+                    logger.writeErrorsToLog(errorInfo, sw);
+                    sw.Close();
+                    //then go back to the previous page
+                    IWebElement goBack = driver.FindElement(By.Id("btnPrev"));
+                    courseButton.Click();
+                    Thread.Sleep(3000);
+                    continue;
+                } finally
+                {
+                    courseButton.Click();
+                    Thread.Sleep(3000);
+                }
             }
         }
     }
